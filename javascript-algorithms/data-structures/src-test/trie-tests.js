31e559dabb0bda4f6a2549e1c741a6e8fc0e19ee
mzu37 <hamlet.darcy@canoo.com>
2013-05-21 22:22:00 +0200

    trie work in progress

--- javascript-algorithms/data-structures/src-test/trie-tests.js
+++ javascript-algorithms/data-structures/src-test/trie-tests.js
@@ -1,5 +1,39 @@
 TrieTestCase = TestCase("TrieTestCase");
 
 TrieTestCase.prototype.testCreate = function () {
+
+    var trie = new Trie();
+
+    assertFalse("should be empty", trie.contains(''));
+    assertFalse("should be empty", trie.contains(undefined));
+    assertFalse("should be empty", trie.contains(null));
+    assertFalse("should be empty", trie.contains('a'));
+    assertFalse("should be empty", trie.contains('ab'));
+    assertFalse("should be empty", trie.contains('abc'));
 };
+
+TrieTestCase.prototype.testInsert = function () {
+
+    var trie = new Trie();
+    trie.insert('a');
+    trie.insert('abc');
+    trie.insert('abcd');
+    trie.insert('abce');
+
+    assertTrue(trie.contains('a'));
+    assertFalse(trie.contains('ab'));
+    assertTrue(trie.contains('abc'));
+    assertTrue(trie.contains('abcd'));
+    assertTrue(trie.contains('abce'));
+    assertFalse(trie.contains('xyz'));
+};
+
+TrieTestCase.prototype.testInsertDuplicates = function () {
+
+    var trie = new Trie();
+    trie.insert('a');
+    trie.insert('a');
+
+    assertEquals(1, trie.children.length);
+}
 
--- javascript-algorithms/data-structures/src/trie.js
+++ javascript-algorithms/data-structures/src/trie.js
@@ -1,17 +1,54 @@
 
-function Trie(value) {
+function Trie(value, isConcrete) {
 
-    this.children = undefined;
-    this.value = undefined;
-    this.isConcrete = false;
+    this.children = [];
+    this.value = value;
+    this.isConcrete = !!isConcrete;
 
 }
 
-Trie.prototype.search = function search(value) {
+Trie.prototype.find = function search(value) {
+    var head, tail, child;
+    if (value === undefined || value === null || value.length === 0) {
+        return false;
+    }
 
+    head = value.charAt(0);
+    tail = value.substring(1);
+
+    child = this.children.filter(function (e) {
+        return e.value === value && e.isConcrete;
+    })[0];// unwrap from array
+
+    if (value.length === 1) {
+        return child;
+    } else if (child === undefined) {
+        return undefined;
+    } else {
+        return child.find(tail);
+    }
 };
-Trie.prototype.insert = function search(value) {
 
+Trie.prototype.contains = function search(value) {
+    return !!this.find(value);
+};
+
+Trie.prototype.insert = function insert(value) {
+    var newTrie, tail;
+    if (value === undefined) {
+        throw new Error("Cannot insert undefined");
+    }
+    if (value === null) {
+        throw new Error("Cannot insert null");
+    }
+    if (value.length == 1) {
+        this.children.push(new Trie(value, true));
+    } else {
+        newTrie = new Trie(value.charAt(0), false);
+        tail = value.substring(1);
+        newTrie.insert(tail) ;
+        this.children.push(newTrie);
+    }
 };
 Trie.prototype.delete = function search(value) {
 
