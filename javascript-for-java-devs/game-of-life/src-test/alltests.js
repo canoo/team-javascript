a76ac64aae5253761d2c49b731e8f3efab516b2b
mzu37 <hamlet.darcy@canoo.com>
2013-05-21 14:13:06 +0200

    OO version of Game of Life

--- javascript-for-java-devs/game-of-life/src-test/alltests.js
+++ javascript-for-java-devs/game-of-life/src-test/alltests.js
@@ -103,3 +103,60 @@ MyTestCase.prototype.testFPVersion_2 = function () {
 
 };
 
+MyTestCase.prototype.testOOVersion_initialState = function () {
+
+    var game = new GameOfLife([
+        [true, false, true, false, true],
+        [true, false, true, true, false],
+        [false, false, true, false, true],
+        [true, false, true, true, false]
+    ]);
+
+    var table = this.render(game.render());
+    assertEquals("cell failed", ['*',  ' ', '*', ' ', '*' ], table[0]);
+    assertEquals("cell failed", ['*',  ' ', '*', '*', ' ' ], table[1]);
+    assertEquals("cell failed", [' ',  ' ', '*', ' ', '*' ], table[2]);
+    assertEquals("cell failed", ['*',  ' ', '*', '*', ' ' ], table[3]);
+};
+
+MyTestCase.prototype.testOOVersion_1 = function () {
+
+    var game = new GameOfLife([
+        [true, false, true, false, true],
+        [true, false, true, true, false],
+        [false, false, true, false, true],
+        [true, false, true, true, false]
+    ]);
+
+    game.advance();
+    var table = this.render(game.render());
+    assertEquals("cell failed", [' ',  ' ', '*', ' ', ' ' ], table[0]);
+    assertEquals("cell failed", [' ',  ' ', '*', ' ', '*' ], table[1]);
+    assertEquals("cell failed", [' ',  ' ', ' ', ' ', '*' ], table[2]);
+    assertEquals("cell failed", [' ',  '*', '*', '*', ' ' ], table[3]);
+
+    game.advance();
+    table = this.render(game.render());
+    assertEquals("cell failed", [' ', ' ', ' ', '*', ' ' ], table[0]);
+    assertEquals("cell failed", [' ', ' ', ' ', ' ', ' ' ], table[1]);
+    assertEquals("cell failed", [' ', '*', ' ', ' ', '*' ], table[2]);
+    assertEquals("cell failed", [' ', ' ', '*', '*', ' ' ], table[3]);
+};
+
+MyTestCase.prototype.testOOVersion_2 = function () {
+    var game = new GameOfLife([
+        [true,  false, false, true,  true],
+        [true,  false, false, false, false],
+        [false, false, false, false, false],
+        [true,  false, false, false, true]
+    ]);
+
+    game.advance();
+    var table = this.render(game.render());
+    assertEquals("cell failed", [' ', ' ', ' ', ' ', ' '], table[0]);
+    assertEquals("cell failed", [' ', ' ', ' ', ' ', ' '], table[1]);
+    assertEquals("cell failed", [' ', ' ', ' ', ' ', ' '], table[2]);
+    assertEquals("cell failed", [' ', ' ', ' ', ' ', ' '], table[3]);
+
+};
+
---
+++ javascript-for-java-devs/game-of-life/src/conway00.js
@@ -0,0 +1,92 @@
+// This is a very rough first draft of implementing a game of life in JavaScript and HTML5
+
+function GameOfLife(initialState) {
+
+    this.board = []; // todo: make this private access somehow
+    var x, y, row, count;
+
+    // game board arrives as a 2d array of booleans, convert to board structure
+    for (y = 0; y < initialState.length; y++) {
+        row = initialState[y];
+        this.board.push([]);
+        for (x = 0; x < row.length; x++) {
+            this.board[y][x] = new Cell(x, y, row[x]);
+        }
+    }
+}
+
+GameOfLife.prototype.render = function() {
+    var y, x, row, cell;
+    var result = [];
+    for (y = 0; y < this.board.length; y++) {
+        row = this.board[y];
+        result.push([]);
+        for (x = 0; x < row.length; x++) {
+            cell = row[x];
+            result[y][x] = !!cell.state; // coerce to boolean
+        }
+    }
+    return result;
+};
+
+GameOfLife.prototype.getNeighbors = function (cell) {
+
+    var result = [];
+    var y = cell.y;
+    var x = cell.x;
+    if (y > 0) {
+        result.push(this.board[y - 1][x - 1]);
+        result.push(this.board[y - 1][x]);
+        result.push(this.board[y - 1][x + 1]);
+    }
+    result.push(this.board[y][x - 1]);
+    result.push(this.board[y][x + 1]);
+    if (y < this.board.length - 1) {
+        result.push(this.board[y + 1][x - 1]);
+        result.push(this.board[y + 1][x]);
+        result.push(this.board[y + 1][x + 1]);
+    }
+    return result;
+};
+
+GameOfLife.prototype.advance = function() {
+    var newBoard = [];
+    var y, x, row, cell;
+
+    for (y = 0; y < this.board.length; y++) {
+        row = this.board[y];
+        newBoard.push([]);
+        for (x = 0; x < row.length; x++) {
+            cell = row[x];
+            newBoard[y][x] = new Cell(cell.x, cell.y, cell.getNextState(this.getNeighbors(cell)));
+        }
+    }
+    this.board = newBoard;
+};
+
+function Cell(x, y, state) {
+    this.x = x;
+    this.y = y;
+    this.state = state;
+}
+
+// live cell with < 2 live neighbours dies
+// live cell with > 3 live neighbours dies
+// live cell with 2 or 3 live neighbours lives
+// dead cell with exactly 3 live neighbours becomes alive
+Cell.prototype.getNextState = function(neighbors) {
+
+    var i, count = 0;
+    for (i = 0; i < neighbors.length; i++) {
+        if (neighbors[i] != undefined && neighbors[i].state) {
+            count = count + 1;
+        }
+    }
+    if (count < 2) {
+        return false;
+    } else if (this.state) {
+        return count <= 3;
+    } else { // must be dead cell
+        return count === 3;
+    }
+};
